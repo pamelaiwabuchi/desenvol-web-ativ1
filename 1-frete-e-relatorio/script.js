4fad2ab36181c1a591dafd7e947c29eaf792d007
Pamela Iwabuchi <199025978+pamelaiwabuchi@users.noreply.github.com>
2026-08-14 10:22:37 -0300

    feat: adiciona função adicionar pedido

--- 1-frete-e-relatorio/script.js
+++ 1-frete-e-relatorio/script.js
@@ -1,1 +1,23 @@
 console.log(document.getElementById("combustivel").value);
+
+console.log(document.getElementById("combustivel").value);
+
+function adicionarPedido() {
+    let combustivel = document.getElementById("combustivel").value;
+    combustivel = combustivel.replace(",", ".");
+    combustivel = Number(combustivel);
+
+    let codigo = document.getElementById("codigo").value;
+    let regiao = document.getElementById("regiao").value;
+
+    let distancia = document.getElementById("distancia").value;
+    distancia = distancia.replace(",",".")
+    distancia = Number(distancia)
+
+    let quantidade = document.getElementById("quantidade").value;
+    quantidade = quantidade.replace(",",".")
+    quantidade = Number(quantidade)
+
+    let sim = document.getElementById("sim").checked;
+
+}
